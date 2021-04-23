1a4ca6091a4e43a40575a29f7e0126f426693ccc
austinandrade <austinmandrade@gmail.com>
2021-04-23 12:36:16 -0400

    Update namespacing for convention

--- app/controllers/flights_passengers_controller.rb
+++ app/controllers/flights_passengers_controller.rb
@@ -1,5 +1,5 @@
 class FlightsPassengersController < ApplicationController
-  def update
+  def destroy
     flight = Flight.find(flights_passenger_params[:id])
     passenger = Passenger.find(flights_passenger_params[:passenger])
     FlightsPassenger.where("flight_id = ?", flight.id).where("passenger_id = ?", passenger.id).destroy_all
